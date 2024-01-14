40f84a8418df410105ad573a4eb30da90397884f
AntonPaskar <anton.p@cgsteam.io>
2024-01-14 18:52:48 +0200

    feat: Try ci cd

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -14,5 +14,6 @@ import { OwnBooksModule } from './modules/own-books/own-books.module';
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(...[TrimMiddleware]).forRoutes('/');
+
   }
 }
