0d7565c9c42ec168c20003aedf3cf1dda73b2771
AntonPaskar <anton.p@cgsteam.io>
2024-01-20 20:17:49 +0200

    hotfix: Fix frontend url

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -24,8 +24,6 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '10mb' }));
 
   app.enableCors({ credentials: true, origin: [process.env.FRONTEND_URL] });
-  console.log(process.env.FRONTEND_URL);
-
 
   app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
